7f1615049c9084a2408a7de8ec16e91b7903e67a
Jason Freeberg <jafreebe@microsoft.com>
2020-02-20 16:53:13 -0800

    Use SQLite DB
    Doing some debugging

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pollsAppDatabase',
-        'USER': os.getenv('DATABASE_USERNAME'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-     }
-}
-
-# Database for local testing/development. Comment this out for deployment
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-#     }
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pollsAppDatabase',
+#         'USER': os.getenv('DATABASE_USERNAME'),
+#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+#         'HOST': os.getenv('DATABASE_HOST'),
+#      }
 # }
+
+# Database for local testing/development. Comment this out for deployment
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 
 
