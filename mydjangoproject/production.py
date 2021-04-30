c9390bfa1de297b7cf93fe3c095b520d93a893e0
Jason Freeberg <jafreebe@microsoft.com>
2021-04-30 14:43:56 -0700

    Update production.py

--- mydjangoproject/production.py
+++ mydjangoproject/production.py
@@ -3,7 +3,7 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], 'www.svzkm.com', 'svzkm.com'] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
